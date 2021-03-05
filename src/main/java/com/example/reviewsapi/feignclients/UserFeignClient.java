8a42387d366d2ab12b55768b7820c48d3371fd17
Larysa Doroshenko <larysad785@gmail.com>
2021-03-05 02:42:17 +0200

    feat: introduce config server

--- src/main/java/com/example/reviewsapi/feignclients/UserFeignClient.java
+++ src/main/java/com/example/reviewsapi/feignclients/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user")
+@FeignClient(value = "user-api")
 public interface UserFeignClient {
     
     @GetMapping(value = "/user/{id}")
